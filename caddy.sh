167d8d21923dc1579c5afee6da946fa97bd0e9aa
sayem314 <sayem314@gmail.com>
2016-03-24 19:05:47 +0600

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -13,7 +13,7 @@
  echo ""
  echo "  Setting up Caddy"
  cd /tmp
- wget -q yesgig.com/downloads/caddy
+ wget -q https://raw.githubusercontent.com/sayem314/Caddy-Web-Server-Installer/master/caddy
  chmod +x caddy
  mv caddy /usr/local/bin
  echo "  Done. run 'caddy' to use Caddy"
