3e6983b7c6e9025be2d511e5bdb5467c0c8b1783
sayem314 <sayem314@gmail.com>
2016-03-24 20:54:19 +0600

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -5,14 +5,9 @@
 #
 #
 
- echo ""
- echo "Removing any previous version"
- rm -f /usr/local/bin/caddy
- echo ""
- echo "  Setting up Caddy"
- cd /tmp
- wget -q https://raw.githubusercontent.com/sayem314/Caddy-Web-Server-Installer/master/caddy
- chmod +x caddy
- mv caddy /usr/local/bin
- echo "  Done. run 'caddy' to use Caddy"
- echo ""
+	# Installing speed test
+	echo ""
+	echo "  Setting up Caddy"
+	wget -q --no-check-certificate https://raw.githubusercontent.com/sayem314/Caddy-Web-Server-Installer/master/caddy && mv caddy /usr/local/bin/caddy && chmod +x /usr/local/bin/caddy
+	echo ""
+	
