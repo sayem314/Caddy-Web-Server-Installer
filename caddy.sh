6c904764a68e71f56927f21fd50c426e70058817
McBochi <McBochi@users.noreply.github.com>
2016-03-29 12:22:13 +0200

    Cleaning code a bit

--- caddy.sh
+++ caddy.sh
@@ -4,17 +4,3 @@
 # Caddy Web Server Installer
 #
 #
-	caddycheck=`[ -e /usr/local/bin/caddy ] && echo "Found" || echo "Not"`
-	if [ "$caddycheck" == 'Found' ]; then
-		echo ""
-		echo "  Removing old Caddy script"
-		rm -f /usr/local/bin/caddy
-	fi
-	echo ""
-	echo "  Setting up Caddy"
-	cd /tmp
-	wget -q https://raw.githubusercontent.com/sayem314/Caddy-Web-Server-Installer/master/caddy
-	chmod +x caddy; mv caddy /usr/local/bin;
-	echo "  Done. run 'caddy' to use Caddy"
-	echo ""
-	exit;
