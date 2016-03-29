cde45572d7427e09379eea1c14bbdabdd07be760
McBochi <McBochi@users.noreply.github.com>
2016-03-29 12:33:56 +0200

    Cleaning code a bit
    Sorry, messed up the previous commit...

--- caddy.sh
+++ caddy.sh
@@ -4,3 +4,17 @@
 # Caddy Web Server Installer
 #
 #
+
+if [[ -e "/usr/local/bin/caddy" ]]; then
+	echo ""
+	echo "  Removing old Caddy script"
+	rm -f /usr/local/bin/caddy
+fi
+echo ""
+echo "  Setting up Caddy"
+cd /tmp
+wget -q https://raw.githubusercontent.com/sayem314/Caddy-Web-Server-Installer/master/caddy
+chmod +x caddy; mv caddy /usr/local/bin;
+echo "  Done. run 'caddy' to use Caddy"
+echo ""
+exit;
